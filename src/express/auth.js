29d2cc29850f9ab99df21b0e9cc88834997815f1
Peter Strøiman <peter@stroiman.com>
2024-10-17 14:11:27 +0200

    Basic username handling

--- src/express/auth.js
+++ src/express/auth.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const router = express.Router();
+
+router.use(bodyParser.urlencoded());
 
 router.get("/login", (req, res) => res.render("auth/login-page"));
 
 router.post("/login", (req, res) => {
-  res.render("auth/login-form");
+  if (req.body.username === "validuser") {
+    res.status(301).header("location", "/me").send();
+  } else {
+    res.status(400).render("auth/login-form");
+  }
 });
 
 module.exports = router;
--- test/express/auth.test.js
+++ test/express/auth.test.js
@@ -7,5 +7,27 @@ describe("/auth routes", () => {
       await supertest(app).get("/auth/login").expect(200);
     });
   });
-  it("Starts with a test", () => {});
+
+  describe("POST /auth/login with bad credential", () => {
+    describe("with good credentials", () => {
+      it("should return a status 303", async () => {
+        const test = supertest(app);
+        await test
+          .post("/auth/login")
+          .send("username=validuser&password=validpassword")
+          .expect(301)
+          .expect("location", "/me");
+      });
+    });
+
+    describe("with bad credentials", () => {
+      it("should return a status 400", async () => {
+        const test = supertest(app);
+        await test
+          .post("/auth/login")
+          .send("username=invaliduser&password=validpassword")
+          .expect(400);
+      });
+    });
+  });
 });
