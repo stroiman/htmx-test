e7e3fe399413c1ac02733c589681110cf962ddb8
Peter Strøiman <peter@stroiman.com>
2024-10-17 13:23:38 +0200

    Skeleton login form with submit button

--- express/auth.js
+++ express/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/login", (req, res) => res.render("login-page"));
+router.get("/login", (req, res) => res.render("auth/login-page"));
+
+router.post("/login", (req, res) => {
+  res.render("auth/login-form");
+});
 
 module.exports = router;
