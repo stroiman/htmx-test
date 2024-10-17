0438b72da9645dc7e36755ea294e466d427dc5d4
Peter Strøiman <peter@stroiman.com>
2024-10-17 11:35:28 +0200

    Added skeleton tailwind configuration

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const app = require("express")();
+const express = require("express");
 const path = require("node:path");
+
+const app = express();
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
