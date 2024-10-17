42cd121f6926288ef4117b4d90401dfe94484829
Peter Strøiman <peter@stroiman.com>
2024-10-17 14:42:49 +0200

    Show validation errors on invalid credentials

--- src/express/auth.js
+++ src/express/auth.js
@@ -5,13 +5,18 @@ const router = express.Router();
 
 router.use(bodyParser.urlencoded());
 
-router.get("/login", (req, res) => res.render("auth/login-page"));
+router.get("/login", (req, res) =>
+  res.render("auth/login-page", { formData: {} }),
+);
 
 router.post("/login", (req, res) => {
   if (req.body.username === "validuser") {
     res.status(301).header("location", "/me").send();
   } else {
-    res.status(400).render("auth/login-form");
+    res.status(400).render("auth/login-form", {
+      formData: { username: req.body.username },
+      invalidCredentials: true,
+    });
   }
 });
 
--- test/express/auth.test.js
+++ test/express/auth.test.js
@@ -1,5 +1,7 @@
 const supertest = require("supertest");
 const app = require("../../src/express");
+const { parse } = require("node-html-parser");
+const { expect } = require("chai");
 
 describe("/auth routes", () => {
   describe("GET /auth/login", () => {
@@ -27,6 +29,28 @@ describe("/auth routes", () => {
           .post("/auth/login")
           .send("username=invaliduser&password=validpassword")
           .expect(400);
+      });
+
+      it("Should keep the username in the text box", async () => {
+        const test = supertest(app);
+        const response = await test
+          .post("/auth/login")
+          .send("username=invaliduser&password=validpassword");
+        const result = parse(response.text);
+        const usernameInput = result.querySelector("#username");
+        const value = usernameInput.getAttribute("value");
+        expect(value).to.equal("invaliduser");
+      });
+
+      it("Should show an error message", async () => {
+        const test = supertest(app);
+        const response = await test
+          .post("/auth/login")
+          .send("username=invaliduser&password=validpassword");
+        const result = parse(response.text);
+        expect(result.textContent).to.contain(
+          "Username or password did not match",
+        );
       });
     });
   });
