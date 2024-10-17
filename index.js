01a9b81ec232e1b596ffcc1ac93a7c93fac1186d
Peter Strøiman <peter@stroiman.com>
2024-10-17 12:02:41 +0200

    Add livereload functionality

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const path = require("node:path");
+var livereload = require("livereload");
 
+var livereloadServer = livereload.createServer({
+  exts: ["pug", "css"],
+});
+livereloadServer.watch([__dirname + "/public", __dirname + "/views"]);
 const app = express();
 
+app.use(require("connect-livereload")());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
